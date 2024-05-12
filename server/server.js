54525b9f50f14ffcc04328bc53ed603b6821f0e3
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-05-12 09:39:21 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect(
-    'mongodb://localhost:27017/societyDB',
+    'mongodb://localhost:27017/societyDB',//Create a database named societyDB in your mongoDb
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
